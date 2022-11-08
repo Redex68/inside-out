47726f2d592e03a6b3c06954eabb1247e89f6431
Marin Petric <marin.petric73@gmail.com>
2022-11-08 14:00:54 +0100

    Updated documentation and added exception throwing

--- Assets/Scripts/HiglightActivator.cs
+++ Assets/Scripts/HiglightActivator.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
+/**
+    <summary>
+        A manager used to control component highlights. Adds listeners to the
+        onGrabEvent and onReleaseEvent events from the player's hands.
+    </summary>
+*/
 
 public class HiglightActivator : MonoBehaviour
 {
+    [Tooltip("The hand GameObjects that contain the left hand's Grabber script that needs to be referenced")]
     public GameObject leftHand;
+    [Tooltip("The hand GameObjects that contain the right hand's Grabber script that needs to be referenced")]
     public GameObject rightHand;
 
+    [Tooltip("A list of all the PC components that can be grabbed and placed.")]
     [SerializeField]
     public List<GameObject> components;
+
+    [Tooltip("A list of all the highlight GameObjects. Must be in the same order as the components list (i.e. the coresponding highlight GameObject must be at the same index as the Grabbable GameObject in the Components list).")]
     [SerializeField]
     public List<GameObject> highlights;
     // Start is called before the first frame update
@@ -22,29 +35,58 @@ public class HiglightActivator : MonoBehaviour
         rightGrabber.onReleaseEvent.AddListener(deactivateHighlight);  
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+/**
+    <summary>
+        Activates the highlight that coresponds to the component that was grabbed.
+    </summary>
+    <exception cref="IndexOutOfRangeException">
+        if the grabbed component exists in the components list but the coresponding index
+        is out of range for the higlights list
+    </exception>
+    <exception cref="NullReferenceException">
+        if the grabbed component exists in the components list but the coresponding highlight
+        GameObject is null in the highlights list
+    </exception>
+*/
     public void activateHighlight(BNG.Grabbable grabbable){
         GameObject component = grabbable.gameObject;
         int indx = components.IndexOf(component);
-        if(indx < 0 || indx >= highlights.Count) return;
-
+        if(indx < 0) return;
+        if(indx >= highlights.Count)
+            throw new IndexOutOfRangeException("Missing the coresponding highlight GameObject for component " + component);
 
         GameObject highlight = highlights[indx];
+        if(highlight == null)
+            throw new NullReferenceException("Missing the coresponding highlight GameObject for component " + component);
+
         highlight.SetActive(true);
         PromptScript.instance.updatePrompt("Acrivated " + highlight);
     }
 
+/**
+    <summary>
+        Deactivates the highlight that coresponds to the component that was released.
+    </summary>
+    <exception cref="IndexOutOfRangeException">
+        if the released component exists in the components list but the coresponding index
+        is out of range for the higlights list
+    </exception>
+    <exception cref="NullReferenceException">
+        if the released component exists in the components list but the coresponding highlight
+        GameObject is null in the highlights list
+    </exception>
+*/
     public void deactivateHighlight(BNG.Grabbable grabbable){
         GameObject component = grabbable.gameObject;
         int indx = components.IndexOf(component);
         if(indx < 0 || indx >= highlights.Count) return;
+        if(indx >= highlights.Count)
+            throw new IndexOutOfRangeException("Missing the coresponding highlight GameObject for component " + component);
 
         GameObject highlight = highlights[indx];
+        if(highlight == null)
+            throw new NullReferenceException("Missing the coresponding highlight GameObject for component " + component);
+            
         highlight.SetActive(false);
         PromptScript.instance.updatePrompt("Deactivated " + highlight);
     }
--- Assets/Scripts/PromptScript.cs
+++ Assets/Scripts/PromptScript.cs
@@ -1,7 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
+/**
+    <summary>
+        A singleton used to control the watch prompt. An instance of the singleton can
+        be accessed through the instance parameter and updatePrompt() can be used to change
+        the prompt that is displayed.
+    </summary>
+*/
 public class PromptScript: MonoBehaviour
 {
     public static PromptScript instance {get; private set;}
@@ -17,15 +25,35 @@ public class PromptScript: MonoBehaviour
         else 
         { 
             instance = this; 
+            TMPtext = GameObject.Find("Watch").GetComponentInChildren<TMPro.TMP_Text>();
         }
-        TMPtext = GameObject.Find("Watch").GetComponentInChildren<TMPro.TMP_Text>();
     }
 
+/**
+    <summary>
+        Fetches the text value of the currently displayed prompt.
+    </summary>
+
+    <returns>
+        The text value of the currently displayed prompt.
+    </returns>
+*/
     public string getPrompt(){
         return TMPtext.text;
     }
 
+/**
+    <summary>
+        Updates the currently displayed prompt with the provided text. The old prompt is
+        overriden.
+    </summary>
+
+    <returns>
+        The new text value of the prompt.
+    </returns>
+*/
     public void updatePrompt(string text){
+        if(text == null) throw new NullReferenceException("Prompt text atempted to be changed to null");
         TMPtext.text = text;
     }
 }
