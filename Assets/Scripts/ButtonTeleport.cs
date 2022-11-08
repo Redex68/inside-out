d06f1eb2ec68ee7ef09c1ef23c783ae2faa7d7be
Marin Petric <marin.petric73@gmail.com>
2022-11-08 14:02:02 +0100

    Fixed prefabs, added larger house for miniature mode and created teleporter to larger house.

---
+++ Assets/Scripts/ButtonTeleport.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTeleport : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private void OnTriggerEnter(Collider other){
+        if(other.tag == "Player"){
+            player.transform.position = new Vector3(-504.106f, 183.744f, 993.669f);
+            player.transform.rotation = Quaternion.Euler(0,-175,0);
+        }
+    }
+}
