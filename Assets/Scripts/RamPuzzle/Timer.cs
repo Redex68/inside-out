3f2cb2e3a159827d0ec0090f15325c24510f207c
BranimirTomeljak <mtomeljak@gmail.com>
2023-01-18 00:00:53 +0100

    add ramPuzzle ending animation

--- Assets/Scripts/RamPuzzle/Timer.cs
+++ Assets/Scripts/RamPuzzle/Timer.cs
@@ -9,11 +9,18 @@ public class Timer : MonoBehaviour
     public float timeLeft = 150f; 
     public int[] cubeSequence = new int[5];
     public bool done = false;
+    public GameObject cube0, cube1;
+    public Vector3[] cubeSnapPosition;
+    public Vector3 cubeSnapRotation = new Vector3(0, 180, -38f);
 
     public void Start()
     {
         List<GameObject> all0Cubes = GameObject.Find("spawnPointCube0").GetComponent<CloneBox0>().all0Cubes;
         List<GameObject> all1Cubes = GameObject.Find("spawnPointCube1").GetComponent<CloneBox1>().all1Cubes;
+        cube0 = GameObject.Find("spawnPointCube0").GetComponent<CloneBox0>().cubeClone;
+        cube1 = GameObject.Find("spawnPointCube1").GetComponent<CloneBox1>().cubeClone;
+        cubeSnapPosition = GameObject.Find("Snapping").GetComponent<SnappingScript>().cubeSnapPosition;
+
         for(int i = 0; i < all0Cubes.Count - 1; i++)
             Destroy(all0Cubes[i]);
         for(int i = 0; i < all1Cubes.Count - 1; i++)
@@ -39,14 +46,13 @@ public class Timer : MonoBehaviour
                 for(int i = 0; i < 5; i++)
                     if(rightOrder[i] == true)
                         counter++;
-                if(counter == 5){
+                if(counter == 1){
                     timerOn = false;
                     done = true;
                     
-                    PromptScript.instance.updatePrompt("Uspješno ste riješili zagonetku!", 2f);
-                    foreach(GameObject obj in GameObject.FindGameObjectsWithTag("cube0")) Destroy(obj);
-                    foreach(GameObject obj in GameObject.FindGameObjectsWithTag("cube1")) Destroy(obj);
-                    TransitionManager.completePuzzle();
+                    PromptScript.instance.updatePrompt("You have successfully solved the puzzle!", 5f);
+                    cubeSnapPosition = GameObject.Find("Snapping").GetComponent<SnappingScript>().cubeSnapPosition;
+                    StartCoroutine(endPuzzle());
                 }
             }
             else{
@@ -72,5 +78,21 @@ public class Timer : MonoBehaviour
         PromptScript.instance.updatePrompt("Vrijeme isteklo! Probaj opet.");
         yield return new WaitForSeconds(3f);
         Start();
+    }
+
+    IEnumerator endPuzzle(){
+        yield return new WaitForSeconds(1f);
+        for(int i = 0; i < 40; i++){
+            Instantiate(i%2==0 ? cube0 : cube1, cubeSnapPosition[0], Quaternion.Euler(cubeSnapRotation));
+            Instantiate(i%2==0 ? cube1 : cube0, cubeSnapPosition[1], Quaternion.Euler(cubeSnapRotation));
+            Instantiate(i%2==0 ? cube0 : cube1, cubeSnapPosition[2], Quaternion.Euler(cubeSnapRotation));
+            Instantiate(i%2==0 ? cube1 : cube0, cubeSnapPosition[3], Quaternion.Euler(cubeSnapRotation));
+            Instantiate(i%2==0 ? cube0 : cube1, cubeSnapPosition[4], Quaternion.Euler(cubeSnapRotation));
+            yield return new WaitForSeconds(0.15f);
+        }
+        TransitionManager.completePuzzle();
+        yield return new WaitForSeconds(3f);
+        foreach(GameObject obj in GameObject.FindGameObjectsWithTag("cube0")) Destroy(obj);
+        foreach(GameObject obj in GameObject.FindGameObjectsWithTag("cube1")) Destroy(obj);
     }
 }
