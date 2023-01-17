0d2230340d87774caf8a9982a45ed16926ac8f77
josipamarkic <jm533@fer.hr>
2023-01-17 14:15:02 +0100

    prefab created

--- Assets/Scripts/cpuPuzzle/cpuPuzzleManager.cs
+++ Assets/Scripts/cpuPuzzle/cpuPuzzleManager.cs
@@ -3,22 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cpuPuzzleManager : Puzzle
+public class cpuPuzzleManager : MonoBehaviour
 {
 
     private bool puzzleActive = false;
-    //private GameObject player;
     private int counter;
-    private GameObject[] cpuPuzzleComponents;
+
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //disable components
-        // cpuPuzzleComponents = GameObject.FindGameObjectsWithTag("task");
-        // foreach(GameObject obj in cpuPuzzleComponents) obj.SetActive(false);
+        
         PromptScript.instance.updatePrompt("Choose the correct logic operation that was applied to get right from left objects!", 3f);
+        SetupPuzzle();
         
     }
 
@@ -36,26 +35,11 @@ public class cpuPuzzleManager : Puzzle
 
     private void SetupPuzzle()
     {
-        //enable components
-        //foreach(GameObject obj in cpuPuzzleComponents) obj.SetActive(true);
+
+        
         puzzleActive = true;
         this.counter = 0;
        
-    }
-
-    private IEnumerator DelayedTeleport()
-    {
-        yield return new WaitForSeconds(1.5f);
-
-        SetupPuzzle();
-        //player.GetComponent<BNG.PlayerTeleport>().TeleportPlayer(new Vector3(-484.75f,186f,1006.78003f), Quaternion.identity);
-    }
-
-    public override void initPuzzle(GameObject player)
-    {
-        //this.player = player;
-        StartCoroutine(DelayedTeleport());
-
     }
 
     public void TaskCorrect() {
@@ -79,11 +63,8 @@ public class cpuPuzzleManager : Puzzle
     public void PuzzleCleared()
     {
         
-        //disable components
-        foreach(GameObject obj in cpuPuzzleComponents) obj.SetActive(false);
         puzzleActive = false;
         PromptScript.instance.updatePrompt("Congratulations! You have beaten the puzzle!", 3);
-        //player.GetComponent<BNG.PlayerTeleport>().TeleportPlayer(new Vector3(0, 5.142f, 0), Quaternion.identity);
         counter = 0;
     }
 }
