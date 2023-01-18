409271ab05aad921bc01a54b315b69ba2114f353
Bruno Perković <perkanperkovic15@gmail.com>
2023-01-19 00:42:46 +0100

    Update StartPosCamera.cs

--- Assets/Scripts/Effects_and_Ending/StartPosCamera.cs
+++ Assets/Scripts/Effects_and_Ending/StartPosCamera.cs
@@ -7,7 +7,7 @@ public class StartPosCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(transform.GetChild(0).gameObject);
+        // Destroy(transform.GetChild(0).gameObject);
     }
 
     // Update is called once per frame
